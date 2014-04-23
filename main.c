1cd6dcd5de750ec769a911b45fd43965dca556e2
jb <jb@jb-WS>
2014-04-23 21:02:06 +0100

    Initial code

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jb
+ *
+ * Created on 23 April 2014, 20:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
